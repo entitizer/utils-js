a6b7e4c1e9c7b6623d99b1971bc7e6aab31fca07
Dumitru Cantea <canteadumitru@gmail.com>
2015-11-25 20:41:23 +0200

    no deps

--- test/test.js
+++ test/test.js
@@ -4,13 +4,8 @@ var utils = require('../lib');
 var assert = require('assert');
 
 describe('utils', function() {
-	it('has libs', function() {
-		assert.ok(utils._);
-		assert.ok(utils.Promise);
-	});
-
 	it('methods', function() {
 		assert.equal('function', typeof utils.isLower);
-		assert.equal('function', typeof utils.atonic);
+		assert.equal('function', typeof utils.isNull);
 	});
 });
